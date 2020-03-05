6d51fd95468a3f5f5a590f635a60beafba79570a
Jermaine Battle <jermainebattle@jermainebattle-macbookpro.roam.corp.google.com>
2020-03-05 13:50:01 -0800

    first commit

---
+++ Components/js/script.js
@@ -0,0 +1,18 @@
+document.writeln('<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>');
+
+
+$(document).ready(function(){ 
+	$(this).alert("jquery working");
+$('img.logo-1').on({
+	mouseenter: function(){
+		$(this).css('width', '200px');
+	},
+	click: function(){
+
+	},
+	mouseleave: function(){
+
+	}
+});
+
+});
